c7de6000814fe4efa081110c13526bb6a600bcf8
Apipol Sukgler <apipol@computerlogy.com>
2018-07-25 10:04:43 +0700

    [Edited]: refactor function to method

--- mhee-golf/src/pointcalculate/pointcalculate.go
+++ mhee-golf/src/pointcalculate/pointcalculate.go
@@ -1,5 +1,22 @@
 package pointcalculate
 
-func PointCalculate(member string, currentPoint int, amount float64) int {
-	return 0
+type Member struct {
+	Level        string
+	CurrentPoint int
+}
+
+func (m Member) PointCalculate(amount float64) int {
+	return m.CurrentPoint + m.CalculatePointByLevel(int(amount))
+
+}
+
+func (m Member) CalculatePointByLevel(amount int) int {
+	levelPointMap := map[string]int{
+		"Gold":     3,
+		"Regular":  1,
+		"Silver":   2,
+		"Non-Rank": 0,
+	}
+
+	return (amount / 200) * levelPointMap[m.Level]
 }
